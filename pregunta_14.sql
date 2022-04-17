bd05bd0d3900f0b996a6ab955c01d785345aeb5b
Angel David Machado <amachadoa@unal.edu.co>
2022-04-17 17:26:46 -0500

    decimocuarta pregunta

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,4 +43,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, AVG(c21) FROM tbl1 A JOIN tbl2 B ON A.K1 = B.K1  WHERE c13 > 400 GROUP BY K0
+SELECT K0, avg(c21) FROM tbl1 A JOIN tbl2 B ON A.K1 = B.K1  WHERE c13 > 400 GROUP BY K0
